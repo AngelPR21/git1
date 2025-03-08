811022794ae416bdfe2aad97b75338fb8e91caa4
Àngel <angpasrod2@alu.edu.gva.es>
2025-03-08 17:58:59 +0100

    Cambio main
    wasd

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("a");
-        System.out.println("vaca");
+        System.out.println("vacawasd");
     }
 }
