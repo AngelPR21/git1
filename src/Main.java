b65a94d8d0299591e707323313e9a01bd23fbdad
angel <angpasrod2@alu.edu.gva.es>
2025-03-08 17:45:23 +0100

    v2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola");
-        System.out.println();
+        System.out.println("a");
         System.out.println("vaca");
     }
 }
